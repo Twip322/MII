c5904b69079979d846b0e177343c7fc2bf98712b
Twip322 <samprp458804@gmail.com>
2021-12-03 14:53:14 +0400

    do 2nd and half of 3rd

--- lab2.py
+++ lab2.py
@@ -1,4 +1,10 @@
-def count_cluster(x,clusterLabels):
+import matplotlib.pyplot as plt
+import random as rand
+
+import numpy as np
+
+
+def count_cluster(x, clusterLabels):
     if clusterLabels[0] <= x <= clusterLabels[3]:
         if clusterLabels[0] <= x <= clusterLabels[1]:
             return (x - clusterLabels[0]) / (clusterLabels[1] - clusterLabels[0])
@@ -10,44 +16,82 @@ def count_cluster(x,clusterLabels):
         return 0
 
 
-def last_cluster(x, clusterLabels):
-    if clusterLabels[0] <= x:
-        if clusterLabels[0] <= x <= clusterLabels[1]:
-            return (x - clusterLabels[0]) / (clusterLabels[1] - clusterLabels[0])
-        if clusterLabels[1] < x:
-            return 1
+weights = {
+    'Узник концлагеря': [30, 38, 42, 44],
+    'Малый вес': [40, 45, 50, 55],
+    'Средний вес': [52, 60, 68, 70],
+    'Большой вес': [69, 72, 80, 95],
+    'Жировое цунами': [92, 98, 100, 112]
+}
 
-    else:
-        return 0
-
+list_of_weight = [rand.randrange(30, 112) for i in range(10)]
 
 
 def clustering(x):
     result = []
-
-    # print(x)
-    result.append(count_cluster(x, [30, 38, 42, 44]))
-    print(f"На {result[0]} узник концлагеря")
-    result.append(count_cluster(x, [40, 45,50,55]))
-    print(f"На {result[1]} малый вес")
-
-    result.append(count_cluster(x, [52, 60, 68, 70]))
-    print(f"На {result[2]} средний вес")
-
-    result.append(count_cluster(x, [69, 72, 80, 95]))
-    print(f"На {result[3]} большой вес")
-
-    result.append(last_cluster(x, [92, 98, 100, 112]))
-    print(f"На {result[4]} жировое цунами")
-
-
-
+    resultW = []
+    result.append(count_cluster(x, weights['Узник концлагеря']))
+    resultW.append('Узник концлагеря')
+    result.append(count_cluster(x, weights['Малый вес']))
+    resultW.append('Малый вес')
+    result.append(count_cluster(x, weights['Средний вес']))
+    resultW.append('Средний вес')
+    result.append(count_cluster(x, weights['Большой вес']))
+    resultW.append('Большой вес')
+    result.append(count_cluster(x, weights['Жировое цунами']))
+    resultW.append('Жировое цунами')
+    iterator=0
+    iterStop=0
     max = result[0]
     for i in result:
         if i > max:
             max = i
+            iterStop+=iterator
+        else:
+            iterator+=1
+    print(
+        "Чёткое значение: {0}, Узник концлагеря: {1}, Малый вес {2}, Средний вес {3}, Большой вес {4}, Жировое цунами {5}, Результат : {6}".format(
+            max, result[0], result[1], result[2], result[3], result[4], resultW[iterStop]))
 
-    print(f"Max {max}")
-    print()
-while True:
-    clustering(int(input('Введите вес:')))
+
+for i in list_of_weight:
+    clustering(i)
+
+U = np.arange(30, 120, 0.1)
+values = []
+
+for plt_set in weights.keys():
+    memberships = [count_cluster(x, weights[plt_set]) for x in U]
+    tmp, = plt.plot(U, memberships, label=plt_set)
+    values.append(tmp)
+plt.legend(handles=values)
+
+
+def crossing():
+    imp1 = []
+    for x in U:
+        imp1.append(min(count_cluster(x, weights['Узник концлагеря']), count_cluster(x, weights['Средний вес']),
+                        count_cluster(x, weights['Жировое цунами'])))
+    return imp1
+
+
+plt.plot(U, crossing(), color='black')
+
+
+def union():
+    imp2 = []
+    for x in U:
+        imp2.append(max(count_cluster(x, weights['Узник концлагеря']), count_cluster(x, weights['Малый вес']),
+                        count_cluster(x, weights['Средний вес']), count_cluster(x, weights['Большой вес']),
+                        count_cluster(x, weights['Жировое цунами'])))
+    return imp2
+
+
+plt.plot(U, union(), color='gray')
+plt.show()
+
+## TODO
+## Захардкодить список
+## Графики
+## Пересечение 1 и 3 и 5, объединение всех
+## Таблицу {Четкое значение, Все степени, результат}
--- lab3.py
+++ lab3.py
@@ -4,7 +4,72 @@ from tkinter import *
 import numpy as np
 from matplotlib import pyplot as plt
 
+## TODO
+## Трапецивидность в условие || +
+## Выбор графиков || +
+## На выбранные клонирование, два один на +, другой на -
+## Объединение полученных клонированных
+
+
 list_functions = []
+list_numbers = []
+
+
+def cloned_func(x, a, b, c, d, isTrapezoid,left):
+    result = []
+    if left == True:
+        if isTrapezoid:
+            for i in x:
+                if a <= i <= d:
+                    if a <= i <= b:
+                        result.append(1)
+                        continue
+                    if b <= i <= c:
+                        result.append(1)
+                        continue
+                    if c <= i <= d:
+                        result.append(1 - (i - c) / (d - c))
+                        continue
+                else:
+                    result.append(0)
+        else:
+            for i in x:
+                if a <= i <= c:
+                    if a <= i <= b:
+                        result.append(1 - (b - i) / (b - a))
+                        continue
+                    if b <= i <= c:
+                        result.append(1 - (i - b) / (c - b))
+                else:
+                    result.append(0)
+        return result
+    else:
+        if isTrapezoid:
+            for i in x:
+                if a <= i <= d:
+                    if a <= i <= b:
+                        result.append(1 - (b - i) / (b - a))
+                        continue
+                    if b <= i <= c:
+                        result.append(1)
+                        continue
+                    if c <= i <= d:
+                        result.append(1)
+                        continue
+                else:
+                    result.append(0)
+        else:
+            for i in x:
+                if a <= i <= c:
+                    if a <= i <= b:
+                        result.append(1 - (b - i) / (b - a))
+                        continue
+                    if b <= i <= c:
+                        result.append(1 - (i - b) / (c - b))
+                else:
+                    result.append(0)
+        return result
+
 
 
 def count_function(x, a, b, c, d, isTrapezoid):
@@ -33,52 +98,78 @@ def count_function(x, a, b, c, d, isTrapezoid):
                     result.append(1 - (i - b) / (c - b))
             else:
                 result.append(0)
-
     return result
 
 
 def clicked():
-    x = np.arange(300)
+
 
     a = int(fn_1_1.get())
     b = int(fn_1_2.get())
     c = int(fn_1_3.get())
     d = int(fn_1_4.get())
-
-    list_functions.append(count_function(x, a, b, c, d, CheckVar1.get()))
+    x = np.arange(d + 10)
+    if d == 0:
+        trap = False
+    else:
+        trap = True
+    list_functions.append(count_function(x, a, b, c, d, trap))
+    list_numbers.append(returnNumbers(x, a, b, c, d, trap))
     listbox_update()
 
 
+def returnNumbers(x, a, b, c, d, trap):
+    nums = [x, a, b, c, d, trap]
+    return nums
+
+
 def show():
-    for i in list_functions:
-        plt.plot(i)
+    z = 0
+    for i in range(list_functions.__len__()):
+        if lbox.curselection().__len__() > z:
+            if i == lbox.index(lbox.curselection()[z]):
+                plt.plot(list_functions[i])
+                z += 1
+    union()
     plt.show()
 
 
 def change():
-    list_functions.pop(lbox.curselection()[0])
-
-    x = np.arange(300)
-
     a = int(fn_1_1.get())
     b = int(fn_1_2.get())
     c = int(fn_1_3.get())
     d = int(fn_1_4.get())
+    x = np.arange(d + 10)
 
-    list_functions.append(count_function(x, a, b, c, d, CheckVar1.get()))
+    if (d == 0):
+        trap = False
+    else:
+        trap = True
+    list_functions.append(count_function(x, a, b, c, d, trap))
+    list_numbers.append(returnNumbers(x, a, b, c, d, trap))
     listbox_update()
+
+
+def clone():
+    nums = list_numbers[lbox.index(lbox.curselection()[0])]
+    imp1=[]
+    x=np.arange(600)
+    for l in x:
+        imp1.append(
+            max(list_functions[lbox.index(lbox.curselection()[0])], list_functions[lbox.index(lbox.curselection()[1])],
+                list_functions[lbox.index(lbox.curselection()[2])]))
+    list_functions.append(cloned_func(nums[0]+100, nums[1], nums[2], nums[3], nums[4], nums[5],True))
+    list_numbers.append(nums)
+    list_functions.append(cloned_func(nums[0] - 100, nums[1], nums[2], nums[3], nums[4], nums[5],False))
+
+    listbox_update()
+
+
 
 
 def del_function():
     list_functions.pop(lbox.curselection()[0])
-    listbox_update()
-
-
-def dop():
-    result = []
-    for i in range(len(list_functions[lbox.curselection()[0]])):
-        result.append(1 - list_functions[lbox.curselection()[0]][i])
-    list_functions.append(result)
+    list_numbers.pop(lbox.curselection()[0])
     listbox_update()
 
 
@@ -89,13 +180,12 @@ def listbox_update():
 
 
 window = Tk()
-window.title("Оценка эффективностиопераций с валютой")
+window.title("Оценка убытков")
 lbl = Label(window, text="Введите данные")
 lbl.grid(column=0, row=0)
-window.geometry('400x250')
+window.geometry('600x250')
 
-# function 1
-lbl_fn1 = Label(window, text="Функция 1:")
+lbl_fn1 = Label(window, text="Функция :")
 lbl_fn1.grid(column=0, row=1)
 
 fn_1_1 = Entry(window, width=5)
@@ -110,28 +200,22 @@ fn_1_3.grid(column=3, row=1)
 fn_1_4 = Entry(window, width=5)
 fn_1_4.grid(column=4, row=1)
 
-CheckVar1 = BooleanVar()
-chk_fn1 = Checkbutton(window, text='Трапецевидная?', variable=CheckVar1)
-chk_fn1.grid(column=5, row=1)
-
-btn = Button(window, text="Клик!", command=clicked)
+btn = Button(window, text="Задать функцию", command=clicked)
 btn.grid(column=2, row=5)
 
-btn_del = Button(window, text="Показать!", command=show)
-btn_del.grid(column=5, row=5)
+btn_del = Button(window, text="Построить график", command=show)
+btn_del.grid(column=3, row=5)
 
-
-btn_change = Button(window, text="Изменить!", command=change)
+btn_change = Button(window, text="Изменить график", command=change)
 btn_change.grid(column=2, row=6)
 
-btn_del = Button(window, text="Удалить!", command=del_function)
+btn_del = Button(window, text="Удалить график", command=del_function)
 btn_del.grid(column=3, row=6)
 
+btn_clone = Button(window, text="Клонировать", command=clone)
+btn_clone.grid(column=4, row=6)
 
-#btn_del = Button(window, text="Дополнить!", command=dop)
-#btn_del.grid(column=3, row=7)
-
-lbox = Listbox(width=15, height=8)
+lbox = Listbox(width=15, height=8, selectmode=MULTIPLE)
 lbox.grid(column=1, row=8)
 
 window.mainloop()
